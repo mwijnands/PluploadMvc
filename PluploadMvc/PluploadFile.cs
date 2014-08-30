a81c17b4dd653fd4026c3244b1e436471d029617
Marcel Wijnands <marcel@shynet.nl>
2014-08-31 01:45:06 +0200

    Added custom implementation of HttpPostedFileBase.

---
+++ PluploadMvc/PluploadFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace XperiCode.PluploadMvc
+{
+    public class PluploadFile : HttpPostedFileBase
+    {
+        private readonly string _fileName;
+        private readonly Stream _fileStream;
+        private readonly string _contentType;
+        private readonly int _contentLength;
+
+        public PluploadFile(string fileName, Stream fileStream, string contentType)
+        {
+            this._fileName = fileName;
+            this._fileStream = fileStream;
+            this._contentType = contentType;
+            this._contentLength = Convert.ToInt32(fileStream.Length);
+        }
+
+        public override string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+        }
+
+        public override Stream InputStream
+        {
+            get
+            {
+                return _fileStream;
+            }
+        }
+
+        public override int ContentLength
+        {
+            get
+            {
+                return _contentLength;
+            }
+        }
+
+        public override string ContentType
+        {
+            get
+            {
+                return _contentType;
+            }
+        }
+        // TODO: override contentlength and contenttype, save these values to a json file when uploaded (next te uploaded file)
+        //       check SaveAs method.
+    }
+}
