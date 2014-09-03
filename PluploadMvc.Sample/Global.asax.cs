75f2e36add6de3bc070c33f0d325e1fc1bb323cc
Marcel Wijnands <marcel@shynet.nl>
2014-09-04 00:41:16 +0200

    Sample 2 working with custom ModelBinder

--- PluploadMvc.Sample/Global.asax.cs
+++ PluploadMvc.Sample/Global.asax.cs
@@ -16,6 +16,8 @@ namespace XperiCode.PluploadMvc.Sample
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ModelBinders.Binders.Add(typeof(PluploadFileCollection), new PluploadModelBinder());
         }
     }
 }
---
+++ PluploadMvc/PluploadModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace XperiCode.PluploadMvc
+{
+    public class PluploadModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            string referenceKey = string.Concat(bindingContext.ModelName, ".Reference");
+            Guid reference = Guid.Parse(bindingContext.ValueProvider.GetValue(referenceKey).AttemptedValue);
+
+            var pluploadContext = controllerContext.HttpContext.GetPluploadContext();
+            var files = pluploadContext.GetFiles(reference).Cast<PluploadFile>().ToList();
+
+            return new PluploadFileCollection(files)
+            {
+                Reference = reference
+            };
+        }
+    }
+}
