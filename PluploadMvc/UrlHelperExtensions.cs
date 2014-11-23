351c0daa6b9b400d70ac94d08c53e45956268b0e
Marcel Wijnands <marcel@shynet.nl>
2014-11-23 22:18:52 +0100

    Added UrlHelperExtensions.

---
+++ PluploadMvc/UrlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace XperiCode.PluploadMvc
+{
+    public static class UrlHelperExtensions
+    {
+        public static string PluploadHandler(this UrlHelper urlHelper, Guid reference)
+        {
+            return urlHelper.Content(string.Concat("~/Plupload.axd?reference=", reference));
+        }
+
+        public static string PluploadHandler(this UrlHelper urlHelper, PluploadFileCollection collection)
+        {
+            return urlHelper.Content(string.Concat("~/Plupload.axd?reference=", collection.Reference));
+        }
+    }
+}
