321aebc7d7f641cd92c6a113f22ae5aa50a80f97
Marcel Wijnands <marcel@shynet.nl>
2015-01-04 02:08:19 +0100

    Check for null arguments in UrlHelperExtensions. Closes #13

---
+++ PluploadMvc.Tests/UrlHelperExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XperiCode.PluploadMvc.Tests
+{
+    [TestClass]
+    public class UrlHelperExtensionsTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void PluploadHandler_Should_Throw_ArgumentNullException_When_Collection_Is_Null()
+        {
+            PluploadFileCollection collection = null;
+            var helper = new UrlHelper(null);
+
+            UrlHelperExtensions.PluploadHandler(helper, collection);
+        }
+    }
+}
--- PluploadMvc/UrlHelperExtensions.cs
+++ PluploadMvc/UrlHelperExtensions.cs
@@ -12,6 +12,11 @@ namespace XperiCode.PluploadMvc
 
         public static string PluploadHandler(this UrlHelper urlHelper, PluploadFileCollection collection)
         {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection", "'collection' is null. When using a ViewModel, you should initialize your ViewModel with new PluploadFileCollection(s).");
+            }
+
             return urlHelper.Content(string.Concat("~/Plupload.axd?reference=", collection.Reference));
         }
     }
